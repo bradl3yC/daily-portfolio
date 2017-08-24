f7ed5b74e67395b53ffcaf9c3da5fe8cc2c5ca20
Bradley Ryan <brad.conl3y@gmail.com>
2017-08-24 16:12:02 -0400

    add filler content

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,8 +4,10 @@ import './index.css';
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <p>Contact Form</p>
+      <div className="contact-div">
+        <h2>Contact!</h2>
+        <p>So you wanna get in touch with the big BM?</p>
+        <p>..me too</p>
       </div>
     );
   }
--- src/components/References/index.js
+++ src/components/References/index.js
@@ -5,9 +5,11 @@ class References extends Component {
   render() {
     return (
       <div>
-        <p>
-          References yo
-        </p>
+        <blockquote>Who better to endorse Bill Murray than.. Bill Murray</blockquote>
+        <h2>Bill Murray:</h2>
+        <p>“Nothing prepared me for being this awesome. It's kind of a shock. It's kind of a shock to wake up every morning and be bathed in this purple light.”</p>
+        <p>“There's a wonderful sense of well-being that begins to circulate . . . up and down your spine. And you feel something that makes you almost want to smile. So what's it like to be me? Ask yourself, ‘What's it like to be me?’ The only way we'll ever know what it's like to be you is if you work your best at being you as often as you can, and keep reminding yourself that's where home is.”</p>
+        <p>“The more relaxed you are, the better you are at everything: the better you are with your loved ones, the better you are with your enemies, the better you are at your job, the better you are with yourself.”</p>
       </div>
     );
   }
